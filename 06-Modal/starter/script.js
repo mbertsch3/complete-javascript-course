39f754b784671ce267b6cc9880fff02b408c9daf
Marci Bertsch <marci.bertsch@gmail.com>
2022-02-28 23:14:16 -0500

    pig game

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,21 +4,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', () => {
-    console.log('Button clicked: ' + btnsOpenModal[i].textContent);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-  const closeModal = () => {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
-
-  btnCloseModal.addEventListener('click', closeModal);
-
-  overlay.addEventListener('click', closeModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
